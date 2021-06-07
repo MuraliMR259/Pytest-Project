7f2a318e350251a17a4ca812ff244ae74efcc032
MuraliMR259 <muralimr259@gmail.com>
2021-06-07 13:00:55 +0530

    add the argparse argument

--- pytestapi/input/csv_to_parquet_aws.py
+++ pytestapi/input/csv_to_parquet_aws.py
@@ -1,17 +1,21 @@
 from io import BytesIO
 import pandas as pd
 import boto3
-import os
 import io
 import site
+
 from importlib import reload
 import sys
-from setuptools.command import easy_install
+# from setuptools.command import easy_install
+# import os
+# import pyarrow
 reload(site)
-import pyarrow
+arg = sys.argv[2]
+print(sys.argv)
 
-input_loc ="pytest-json-and-csv-data/input/Customers.csv" and "pytest-json-and-csv-data/input/Customer Address.csv" and "pytest-json-and-csv-data/input/customer order details.csv" and "pytest-json-and-csv-data/input/customer order master.csv" and "pytest-json-and-csv-data/input/payment details.csv"
-output_loc = "pytest-json-and-csv-data/output/"
+input_loc = sys.argv[1]
+
+output_loc = sys.argv[2]
 
 input_bucket = input_loc.split('/', 1)[0]
 object_key = input_loc.split('/', 1)[1]
@@ -27,9 +31,10 @@ parquet_buffer = BytesIO()
 s3_resource = boto3.resource('s3')
 df.to_parquet(parquet_buffer, index=False)
 
-#s3_resource.Object(output_loc_bucket, output_loc_prefix + 'order master' + '.parquet').put(Body=parquet_buffer.getvalue())
 
-lst = ['customer','customer address','customer order details','customer order master','payment details']
+lst = ['customer']
 
 for i in lst:
-    s3_resource.Object(output_loc_bucket, output_loc_prefix + i + '.parquet').put(Body=parquet_buffer.getvalue())
+        s3_resource.Object(output_loc_bucket, output_loc_prefix + i + '.parquet').put(Body=parquet_buffer.getvalue())
+
+
